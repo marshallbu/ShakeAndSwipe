2180f9e599bdbe2c0dc0cff4b7f019da7fa2238a
Marshall Upshur <marshall@mbucreations.com>
2015-01-23 16:33:18 -0800

    - initial commit playing with gestures
    Signed-off-by: Marshall Upshur <marshall@mbucreations.com>

---
+++ ShakeAndSwipe/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  ShakeAndSwipe
+//
+//  Created by Marshall Upshur on 1/21/15.
+//  Copyright (c) 2015 Marshall Upshur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        
+        self.view.addGestureRecognizer(swipeUp)
+        
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        if event.subtype == UIEventSubtype.MotionShake {
+            println("user shook")
+        }
+    }
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+                case UISwipeGestureRecognizerDirection.Right:
+                    println("user swiped right")
+                case UISwipeGestureRecognizerDirection.Up:
+                    println("user swiped up")
+                default:
+                    break
+                
+            }
+            
+        }
+        
+        
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
